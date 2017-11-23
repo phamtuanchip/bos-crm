34283aa6cf4e2d22441cad01625418c3af15ad65
Hoang Duc <hoangvanduc101095@gmail.com>
2017-11-23 08:55:10 +0700

    Thay đổi project
    a

---
+++ src/components/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+  <footer>
+  <p>{{aaa}}  {{title}}</p>
+  </footer>
+</template>
+
+<script>
+import{bus} from '../main';
+export default {
+props:{
+  title:{
+    type:String
+  }
+},
+  data () {
+    return {
+    aaa:'Xin chao'
+    }
+  }
+  ,created(){
+    bus.$on('titleChange',(data)=>{
+      this.title=data;
+    });
+  }
+}
+</script>
+<style scoped>
+footer{
+background: #222;
+padding: 6px;
+}
+p{
+color:lightgreen;
+text-align: center;
+}
+</style>
