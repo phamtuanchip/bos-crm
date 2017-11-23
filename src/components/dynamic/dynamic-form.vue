ff075298417d23025b7bfd1c4ea7d623a9a556e1
iVodkabulary <anhdqse03046@fpt.edu.vn>
2017-11-23 09:02:59 +0700

    Dynamic form
    https://www.npmjs.com/package/vue-dynamic-forms

---
+++ src/components/dynamic/dynamic-form.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <!--
+        custom html element
+        custom atribute formdata is required
+     -->
+    <dynamic-form :formdata="formdata"></dynamic-form>
+  </div>
+</template>
+
+<script>
+  //import the library
+  import dynamicForm from 'vue-dynamic-forms'
+
+  import { demoDynamicForm } from './form'
+
+  export default {
+
+    name: 'demo',
+    components:{
+      //add the dynamicForm to the components section
+      dynamicForm
+    }
+    data () {
+      return {
+        //get the configuration and initialize it
+        formdata:demoDynamicForm()
+      };
+    }
+  };
+</script>
+
+<style lang="css" scoped>
+</style>
